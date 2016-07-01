cec56ce6fb4c00d10e0a632cc608dc39512d9630
Meghdut Sengupta <sengupta.meghdut@gmail.com>
2016-07-01 18:54:47 +0530

    Minor fixes

--- ShopCrawler/spiders/shoppy.py
+++ ShopCrawler/spiders/shoppy.py
@@ -5,32 +5,34 @@ from scrapy.selector import Selector
 from scrapy.spiders import Spider
 from lxml import html
 
-
-        
-
 class ShopSpider(CrawlSpider):
 
 	name = 'shoppy'
 	allowed_domains = ["www.shopping.com"]
 	
 	keyword = raw_input("Enter desired keyword:")	
-	page_number = raw_input("Enter desired page_number:")
+	page_number = raw_input("Enter desired page number:")
 
+	def set_urls(keyword, page_number):
+		url = "http://www.shopping.com/products?KW={0}" .format(keyword) 
+		url2 = "http://www.shopping.com/products~PG-{0}?KW={1}" .format(page_number, keyword)
 	
-	url = "http://www.shopping.com/products?KW={0}" .format(keyword) 
-	url2 = "http://www.shopping.com/products~PG-{0}?KW={1}" .format(page_number, keyword)
-	
-	if page_number == None:
-		start_urls = [url]
-	else:	
-		start_urls=[url, url2]
+		if keyword == None:
+			print "Error 404: Not found."
+		elif page_number == None:
+			start = [url]
+			return start
+		else:	
+			start = [url, url2]
+			return start
 
+	start_urls = set_urls(keyword, page_number) 
 	rules = (Rule(SgmlLinkExtractor(allow = ['\\/info']), callback=('parse_item'), follow=True),)
 
 	def parse_item(self, response):
 		sel = Selector(response)
 		sites = sel.xpath('//div[@class="contentInner  singleModelContainer "]')
-		print sites
+		
 		items = []
 
 		for site in sites:
@@ -42,3 +44,5 @@ class ShopSpider(CrawlSpider):
 			items.append(item)
 
 		return items
+
+	
