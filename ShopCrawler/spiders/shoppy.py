78c60afc8142a57ef60b428d12dc8036dd454c67
Meghdut Sengupta <sengupta.meghdut@gmail.com>
2016-07-06 19:58:40 +0530

    minor changes

--- ShopCrawler/spiders/shoppy.py
+++ ShopCrawler/spiders/shoppy.py
@@ -4,6 +4,7 @@ from ShopCrawler.items import ShopcrawlerItem
 from scrapy.selector import Selector
 from scrapy.spiders import Spider
 from lxml import html
+from unittest.case import TestCase
 
 class ShopSpider(CrawlSpider):
 
@@ -13,36 +14,41 @@ class ShopSpider(CrawlSpider):
 	keyword = raw_input("Enter desired keyword:")	
 	page_number = raw_input("Enter desired page number:")
 
+	if not keyword:
+		print "Error 404: Not found."
+
 	def set_urls(keyword, page_number):
-		url = "http://www.shopping.com/products?KW={0}" .format(keyword) 
-		url2 = "http://www.shopping.com/products~PG-{0}?KW={1}" .format(page_number, keyword)
-	
-		if keyword == None:
-			print "Error 404: Not found."
-		elif page_number == None:
-			start = [url]
+		 
+		if not page_number:
+			start = ["http://www.shopping.com/products?KW={0}" .format(keyword)]
 			return start
 		else:	
-			start = [url, url2]
+			start = ["http://www.shopping.com/products~PG-{0}?KW={1}" .format(page_number, keyword)]
 			return start
 
-	start_urls = set_urls(keyword, page_number) 
-	rules = (Rule(SgmlLinkExtractor(allow = ['\\/info']), callback=('parse_item'), follow=True),)
-
-	def parse_item(self, response):
-		sel = Selector(response)
-		sites = sel.xpath('//div[@class="contentInner  singleModelContainer "]')
+	if keyword:
 		
-		items = []
+		start_urls = set_urls(keyword, page_number) 
+	
+		rules = (Rule(SgmlLinkExtractor(allow = ['\\/info']), callback=('parse_item'), follow=True),)
 
-		for site in sites:
+	
+		def parse_item(self, response):
+			sel = Selector(response)
+			sites = sel.xpath('//div[@class="contentInner  singleModelContainer "]')
+		
+			items = []
+
+			for site in sites:
 			
-			item = ShopcrawlerItem()
+				item = ShopcrawlerItem()
 
-			item['title'] = "".join(site.xpath('//div[@class="hproduct"]/h1/text()').extract())
-			item['price'] = "".join(site.xpath('//div[@class="productPriceBox"]/span/text()').extract())
-			items.append(item)
+				item['title'] = "".join(site.xpath('//div[@class="hproduct"]/h1/text()').extract())
+				item['price'] = "".join(site.xpath('//div[@class="productPriceBox"]/span/text()').extract())
+				items.append(item)
 
-		return items
+			return items
+
+
 
 	
